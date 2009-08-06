d784cef23623c960ab12cf4b0c3432ac47a78e87
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-08-06 09:36:09 +0900

    initial commit

---
+++ lib/F4.pm
@@ -0,0 +1,33 @@
+package F4;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+F4 -
+
+=head1 SYNOPSIS
+
+  use F4;
+
+=head1 DESCRIPTION
+
+F4 is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
